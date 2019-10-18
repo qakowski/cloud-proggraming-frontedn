cd307836e0ce8b0b5c61f3212d025fcf679ed0fa
Krystian Kuczkowski <krystian.kuczkowski@outlook.com>
2019-10-18 17:15:27 +0200

    add the build

--- src/app/components/building-form/building-form.component.ts
+++ src/app/components/building-form/building-form.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./building-form.component.scss']
 })
 export class BuildingFormComponent implements OnInit {
+  imageSrc: string;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  readUrl(event) {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imageSrc = e.target.result;
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  send() {
+    
   }
 
 }
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor() { }
+  url = '';
+
+  constructor(private http: HttpClient) { }
+
+  sendImage(image) {
+    const formData = new FormData();
+    formData.append('image', image);
+
+    return this.http.post(this.url, formData);
+  }
 }
