9786609f8a4eb7ce387afeea7b79b29a3816c587
Krystian Kuczkowski <krystian.kuczkowski@outlook.com>
2019-10-18 16:20:04 +0200

    add some basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BuildingFormComponent } from './components/building-form/building-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BuildingFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/building-form/building-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-building-form',
+  templateUrl: './building-form.component.html',
+  styleUrls: ['./building-form.component.scss']
+})
+export class BuildingFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
